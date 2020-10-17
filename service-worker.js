5308c1f457f35a5ac7bd7e839816f4132b5fed35
Alona <aionovaionova@gmail.com>
2020-10-17 18:01:34 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.088a5fcd4c2c4d8f3896026f26515eeb.js"
 );
 
 self.addEventListener('message', (event) => {
